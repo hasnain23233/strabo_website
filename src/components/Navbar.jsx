37ad58d1136aec527680463366049cbe75ca277b
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-18 11:59:56 +0500

    complete responsive the navbar of the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,31 @@ import Logo from '../assets/logo.png';
 import DropdownPlatfom from './DropdownPlatfom';
 import DropdownResourse from './DropdownResourse';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 
 const Navbar = () => {
+    const [menu, setMenu] = useState(true)
+    const handleMenuBtn = () => {
+        setMenu(!menu)
+    }
 
     return (
         <div className="border-b-2 border-gray-100 font-inter">
-            <header className="h-16 w-11/12 m-auto flex justify-between items-center">
-                <div className="flex items-center gap-2 w-2/12">
-                    <img src={Logo} alt="Logo" />
-                    <Link to={'/'} className="text-xl font-semibold tracking-wider text-gray-800">strabo</Link>
+            <header className=" py-3     w-11/12 m-auto flex lg:justify-between lg:flex-row flex-col gap-2 lg:items-center">
+                <div className="flex items-center gap-2 lg:w-2/12 justify-between">
+                    <p className='flex items-center gap-2 lg:w-2/12 w-8/12'>
+                        <img src={Logo} alt="Logo" />
+                        <Link to={'/'} className="text-xl font-semibold tracking-wider text-gray-800">strabo</Link>
+                    </p>
+                    <p onClick={handleMenuBtn} className='lg:hidden block'>
+                        <FontAwesomeIcon icon={faBars} className="text-2xl text-gray-700" />
+                    </p>
                 </div>
 
-                <ul className="w-6/12 text-sm flex justify-evenly items-center gap-4 relative">
+                <ul className={`w-6/12 text-sm flex justify-evenly lg:flex-row flex-col lg:items-center ${menu ? 'hidden lg:flex' : 'flex'} gap-4 relative`}>
                     <li>
                         <DropdownPlatfom />
                     </li>
@@ -34,7 +45,7 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                <div className="w-2/12 flex justify-end text-sm text-gray-600">
+                <div className={`lg:w-2/12 sm:w-5/12 w-11/12 flex lg:justify-end text-sm ${menu ? 'hidden lg:flex' : 'flex'}  text-gray-600`}>
                     <button className="border-2 font-semibold tracking-wider px-5 py-2 rounded-full hover:bg-gray-50">
                         Launch app
                     </button>
